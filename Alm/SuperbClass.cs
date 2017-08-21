9bf2bed874a1a23c2e61e0d895abc8f58c2544f9
Raphaël DUCOM <raphael.ducom@gmail.com>
2017-08-21 22:43:33 +0200

    Adds some usefull notcovered code

--- Alm/SuperbClass.cs
+++ Alm/SuperbClass.cs
@@ -3,5 +3,8 @@
     public class SuperbClass
     {
 		public int TheAnswer() => 42;
-    }
+
+		public string TheQuestion() => "the Ultimate Question of Life, The Universe, and Everything";
+
+	}
 }
